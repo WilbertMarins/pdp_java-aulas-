59c2490ad96b0c7a9a466b7fde0b110a85af1cbc
Wilbert Marins <wilbertluis2014@gmail.com>
2018-03-05 15:02:10 -0300

    Create conversao_execucao.java

---
+++ conversao_execucao.java
@@ -0,0 +1,22 @@
+
+public class Execucao_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x;
+		float y = 35.23f;
+		int z = 10;
+		x = y + z;
+		System.out.print("x = " + x);
+		System.out.println();
+		
+		
+		double a = 10;
+		float b = 35.23f;
+		int c;
+		c = (int) (a + b);
+		System.out.print("c = " + c);
+
+	}
+
+}
