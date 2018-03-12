c9a85a94824cdd59328459e1bfe4caa242516fa5
Wilbert Marins <wilbertluis2014@gmail.com>
2018-03-12 14:52:20 -0300

    voa

---
+++ arrays.java
@@ -0,0 +1,16 @@
+package execucao;
+public class Execucao {
+	public static void main(String[] args) {
+		String[] bichanos = new String[6];
+		bichanos[0]="Tom";
+		bichanos[1]="Garfield";
+		bichanos[2]="Mortin da Silva";
+		bichanos[3]="Rick";
+		bichanos[4]="Morty";
+		bichanos[5]="Otite";
+		
+		for(int i = 0; i< bichanos.length;i++) {
+			System.out.println(bichanos[i]);
+		}
+	}
+}
